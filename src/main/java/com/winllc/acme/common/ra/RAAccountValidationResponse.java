75b60f07a4a01680a4eaac10d2ecaf996abeb7ea
Davd Winters <jrmints22@gmail.com>
2020-07-25 12:32:57 -0400

    updates

---
+++ src/main/java/com/winllc/acme/common/ra/RAAccountValidationResponse.java
@@ -0,0 +1,22 @@
+package com.winllc.acme.common.ra;
+
+public class RAAccountValidationResponse {
+    private boolean valid;
+    private String message;
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
