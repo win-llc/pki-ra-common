69d415817d26b3a7f5ca82a8724f97e255072c01
Davd Winters <jrmints22@gmail.com>
2020-07-28 14:10:50 -0400

    updates to unit tests bug fixes from running unit tests

--- src/main/java/com/winllc/acme/common/ra/RACertificateIssueRequest.java
+++ src/main/java/com/winllc/acme/common/ra/RACertificateIssueRequest.java
@@ -9,18 +9,21 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RACertificateIssueRequest extends RACertificateRequest {
+    private Long existingCertificateRequestId = null;
     private String accountKid;
     private String subjectNameRequest;
     private String csr;
     private String dnsNames;
+    private String source;
 
     private RACertificateIssueRequest(){}
 
-    public RACertificateIssueRequest(String accountKid, String csr, String dnsNames, String certAuthorityName) {
+    public RACertificateIssueRequest(String accountKid, String csr, String dnsNames, String certAuthorityName, String source) {
         super(certAuthorityName);
         this.accountKid = accountKid;
         this.csr = csr;
         this.dnsNames = dnsNames;
+        this.source = source;
     }
 
     @JsonIgnore
@@ -64,4 +67,19 @@ public class RACertificateIssueRequest extends RACertificateRequest {
         this.dnsNames = dnsNames;
     }
 
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Long getExistingCertificateRequestId() {
+        return existingCertificateRequestId;
+    }
+
+    public void setExistingCertificateRequestId(Long existingCertificateRequestId) {
+        this.existingCertificateRequestId = existingCertificateRequestId;
+    }
 }
