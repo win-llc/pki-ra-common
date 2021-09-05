55187b1243413a17dd80ababfa544903ab0e1ed5
Davd Winters <jrmints22@gmail.com>
2021-09-05 12:28:57 -0400

    ignore json owner account

--- src/main/java/com/winllc/acme/common/domain/AccountOwnedEntity.java
+++ src/main/java/com/winllc/acme/common/domain/AccountOwnedEntity.java
@@ -1,5 +1,8 @@
 package com.winllc.acme.common.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface AccountOwnedEntity {
+    @JsonIgnore
     Account getOwnerAccount();
 }
