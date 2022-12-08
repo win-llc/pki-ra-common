574f4ce81afdcc357db0633e50793f95492cf0fc
David Winters <dave.t.winters@gmail.com>
2022-12-08 14:19:13 -0500

    more permissive account request

--- src/main/java/com/winllc/acme/common/model/requestresponse/AccountRequest.java
+++ src/main/java/com/winllc/acme/common/model/requestresponse/AccountRequest.java
@@ -4,17 +4,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nimbusds.jose.JWSObject;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.text.ParseException;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
 public class AccountRequest {
     //optional
     private String status;
     //optional
     private String[] contact;
     //optional
-    private Boolean termsOfServiceAgreed;
+    private Boolean termsOfServiceAgreed = true;
     //optional
     private Boolean onlyReturnExisting;
     //optional
@@ -26,53 +30,6 @@ public class AccountRequest {
     public AccountRequest() {
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String[] getContact() {
-        return contact;
-    }
-
-    public void setContact(String[] contact) {
-        this.contact = contact;
-    }
-
-    public Boolean getTermsOfServiceAgreed() {
-        return termsOfServiceAgreed;
-    }
-
-    public void setTermsOfServiceAgreed(Boolean termsOfServiceAgreed) {
-        this.termsOfServiceAgreed = termsOfServiceAgreed;
-    }
-
-    public Boolean getOnlyReturnExisting() {
-        return onlyReturnExisting;
-    }
-
-    public void setOnlyReturnExisting(Boolean onlyReturnExisting) {
-        this.onlyReturnExisting = onlyReturnExisting;
-    }
-
-    public ExternalAccountBinding getExternalAccountBinding() {
-        return externalAccountBinding;
-    }
-
-    public void setExternalAccountBinding(ExternalAccountBinding externalAccountBinding) {
-        this.externalAccountBinding = externalAccountBinding;
-    }
-
-    public String getResource() {
-        return resource;
-    }
-
-    public void setResource(String resource) {
-        this.resource = resource;
-    }
 
     @JsonIgnore
     public JWSObject buildExternalAccountJWSObject() throws ParseException {
